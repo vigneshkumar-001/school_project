cd34625a3b727a45b32f27f9ab9d3f875400dba6
appfeni <app@fenizotechnologies.com>
2025-06-26 17:14:38 +0530

    new Init

--- README.md
+++
@@ -1,2 +0,0 @@
-# school_project
-st joseph school app 
---
+++ st_school_project/android/app/src/main/kotlin/com/fenizo/st_school_project/st_school_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fenizo.st_school_project.st_school_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
